60372ce7370713216b2662a40344ac5e056d875c
BobmoKlat <yakir123hehr@gmail.com>
2024-12-12 02:05:54 +0200

    a fully working project that convert numbers to diff bases. small cosmetics

--- Matala 0/src/Ex1Main.java
+++ Matala 0/src/Ex1Main.java
@@ -1,57 +1,50 @@
 
 import java.util.Scanner;
 
-/**
- * Intro2CS, Ex1 - very basic "main template"
- * Make sure your implementation of this main performs as the Ex1Sol.jar solution implement all needed functions.
- *
- */
 public class Ex1Main {
     public static void main(String[] args) {
         int base , base1 ,base2;
         String stbase;
         Scanner sc = new Scanner(System.in);
         String num1 = "", num2="", quit = "quit";
-        while (!num1.equals(quit) && !num2.equals(quit)) {
+        while (!num1.equals(quit) && !num2.equals(quit)) {                   //loop in cases of user want to quit, enter wrong num or want to start again.
             System.out.println();
             System.out.println("Ex1 class solution:");
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
-            num1 = sc.next();
+            num1 = sc.next();                                               //first number input
 
             if (!num1.equals("quit")) {
-                if(assignments.ex1.Ex10.isNumber(num1))
+                if(assignments.ex1.Ex10.isNumber(num1))                     //check if number is valid
                     System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
                 else{
+                    System.out.println("404 ERROR! Enter a new string");
                     continue;
                 }
-                    num2 = sc.next();
+                    num2 = sc.next();                                       //second input
                 if (!num2.equals("quit")){
-                    if (assignments.ex1.Ex10.isNumber(num2)){
+                    if (assignments.ex1.Ex10.isNumber(num2)){               //check if number is valid
                         System.out.println("Type the base you want to convert to? [2,16]. Enter A-G for Base 10-16!");
-                        stbase = sc.next();
-                        //convert the string base to int.
-                        if (!(stbase.length() > 1))
-                            base = assignments.ex1.Ex10.inputBase(stbase); //convert the base to int.
                         else {                                             //if the input base isnt valid.
                             System.out.println("Base is not valid to me!!!" + " Try again");
                             continue;
                         }
-                        base1 = assignments.ex1.Ex10.theNumberBase(num1);
-                        base2 = assignments.ex1.Ex10.theNumberBase(num2);
+                        base1 = assignments.ex1.Ex10.theNumberBase(num1);       //Converting String to Int
+                        base2 = assignments.ex1.Ex10.theNumberBase(num2);       //SAME!^
 
                         String []str = num1.split("b");            //splitting the string for tow and check if valid.
-                        String []str2 = num2.split("b");
+                        String []str2 = num2.split("b");           //splitting the string for tow and check if valid.
                         String number = str[0];
                         String number2 = str2[0];
 
-
-                        System.out.println("the first converted number is- "+num1+" in base "+base1+ " and it converted to: "+assignments.ex1.Ex10.converToBase(assignments.ex1.Ex10.converToTen(num1,base1),base));
-                        System.out.println("the second converted number is-"+num2+" in base "+base2+" and it converted to: "+assignments.ex1.Ex10.converToBase(assignments.ex1.Ex10.converToTen(num2,base2),base));
+                        //Calling for the functions
+                        System.out.println("the first converted number is - "+num1+" in base "+base1+ " and it converted to: "+assignments.ex1.Ex10.converToBase(assignments.ex1.Ex10.converToTen(num1,base1),base));
+                        System.out.println("the second converted number is -"+num2+" in base "+base2+" and it converted to: "+assignments.ex1.Ex10.converToBase(assignments.ex1.Ex10.converToTen(num2,base2),base));
                         System.out.println("Max index in-"+ num1+" is: "+ assignments.ex1.Ex10.maxIndex(str));
                         System.out.println("Max index in-"+ num2+" is: "+ assignments.ex1.Ex10.maxIndex(str2));
                         System.out.println("are equals? "+ assignments.ex1.Ex10.equals(number,number2));
                     }
                     else{
+                        System.out.println("Enter  new strings ");
                         continue;
                     }
                 }
